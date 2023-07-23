c46bda11c6159ccfd4b4a6721ff9a7681b7e9ad9
Evelyn Luu <evelyn.luu@corelogic.com.au>
2023-07-23 17:28:17 +1000

    Adding nhom_sp column

--- SX_Daily_1.sql
+++ SX_Daily_1.sql
@@ -14,11 +14,13 @@ SELECT
     ten_hang ,
     ma_nhom ,
     ma_hang ,
+    nhom_sp ,
     dvt ,
     kho ,
     '-' pham_cap ,
     tt ,
-    sl_qk
+    sl_qk,
+    sl_tt
 INTO #tieuhao_raw
 FROM ODS.dbo.FACT_BANGKETIEUHAO_MES
 WHERE cd like N'%Nghiền liệu%'
@@ -39,11 +41,13 @@ SELECT
     ten_hang ,
     ma_nhom ,
     ma_hang ,
+    '-' nhom_sp ,
     dvt ,
     kho ,
     pham_cap ,
     tt ,
-    sl
+    sl,
+    sl_sx
 INTO #nhapve_raw
 FROM ODS.dbo.FACT_BANGKENHAPTHANHPHAM_MES
 
@@ -63,14 +67,16 @@ SELECT
 	ten_hang ,
 	ma_nhom ,
 	ma_hang ,
+    nhom_sp ,
 	dvt ,
 	kho ,
 	pham_cap ,
 	tt ,
-	ROUND(SUM(sl_qk),2) sl
+	ROUND(SUM(sl_qk),2) sl_th,
+	ROUND(SUM(sl_tt),2) sl_kh
 INTO #tieuhao_processed
 FROM #tieuhao_raw
-GROUP BY bang_goc , so_phieu , lsx , sp , x_product_code , cd , ngay , ten_hang , ma_nhom , ma_hang , dvt , kho , pham_cap , tt
+GROUP BY bang_goc , so_phieu , lsx , sp , x_product_code , cd , ngay , ten_hang , ma_nhom , ma_hang , nhom_sp , dvt , kho , pham_cap , tt
 
 ------------------------------------------------------------------------
 -- Tạo bảng "Nhập Về processed"
@@ -88,24 +94,41 @@ SELECT
 	ten_hang ,
 	ma_nhom ,
 	ma_hang ,
+    nhom_sp ,
 	dvt ,
 	kho ,
 	pham_cap ,
 	tt ,
-	ROUND(SUM(sl),2) sl
+	ROUND(SUM(sl),2) sl_th,
+	ROUND(SUM(sl_sx),2) sl_kh
 INTO #nhapve_processed
 FROM #nhapve_raw
-GROUP BY bang_goc , so_phieu , lsx , sp , x_product_code , cd , ngay , ten_hang , ma_nhom , ma_hang , dvt , kho , pham_cap , tt
+GROUP BY bang_goc , so_phieu , lsx , sp , x_product_code , cd , ngay , ten_hang , ma_nhom , ma_hang , nhom_sp , dvt , kho , pham_cap , tt
 
 ------------------------------------------------------------------------
 -- Tạo bảng "SX_Daily_1"
 ------------------------------------------------------------------------
+IF OBJECT_ID('ODS.dbo.SX_Daily_1') IS NOT NULL
+    DROP TABLE ODS.dbo.SX_Daily_1;
+
 IF OBJECT_ID('tempdb..#union_all') IS NOT NULL
     DROP TABLE tempdb..#union_all;
 WITH X AS (
     SELECT * FROM #tieuhao_processed UNION ALL SELECT * FROM #nhapve_processed
+),
+Y AS (
+    SELECT
+        m.mmtb,
+        X.*
+    FROM X
+    LEFT JOIN ODS.dbo.FACT_BANGKEMAYMOC_MES m 
+        ON X.cd = m.cd
+        AND X.ngay = m.ngay
+        AND X.so_phieu = m.so_phieu
+        AND X.lsx = m.lsx
+        AND X.sp = m.sp
 )
 
 SELECT * 
 INTO ODS.dbo.SX_Daily_1
-FROM X
+FROM Y
